44069568c39bc624d407bffdc4b6594c58eafa5d
DuckonDuck <116538168+papawolfexe@users.noreply.github.com>
2023-04-08 17:09:44 -0700

    C# programs for a game

---
+++ ProceduralGround.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using UnityEngine;
+ 
+public class ProceduralGround : MonoBehaviour {
+ 
+   public GameObject GeneratedObjectHolder;
+   public Material PrimaryMaterial;
+   public Material SecondaryMaterial;
+ 
+   public int StartXLocation = 0;
+   public int StartZLocation = 0;
+ 
+   public int CountX = 20;
+   public int CountZ = 20;
+ 
+   public float CubeSize = 2.0f;
+   public float CubeSpacingMultiplier = 0.01f;
+ 
+   public void Start() {
+       var cubeSpacing = CubeSize + CubeSize * CubeSpacingMultiplier;
+       var random = new System.Random();
+       int layerGround = LayerMask.NameToLayer("Ground");
+ 
+ 
+      
+       for(var x = 0; x < CountX; x++) {
+           for(var z = 0; z < CountZ; z++) {
+               var position = new Vector3(
+                   -1 * x * cubeSpacing,
+                   ((float)random.NextDouble()) * CubeSize,
+                   z * cubeSpacing
+               );
+               var cube = GameObject.CreatePrimitive(
+                   PrimitiveType.Cube
+               );
+               cube.transform.position = position;
+               cube.layer = layerGround;
+               cube.transform.localScale = new Vector3(
+                   CubeSize,
+                   CubeSize,
+                   CubeSize
+               );
+               cube.transform.parent = GeneratedObjectHolder.transform;
+ 
+               var cubeRenderer = cube.GetComponent<Renderer>();
+               if(random.NextDouble() < 0.1) {
+                   cubeRenderer.material = SecondaryMaterial;
+               } else {
+                   cubeRenderer.material = PrimaryMaterial;
+               }
+           }
+       }
+   }
+}
