3fd614bb024f5bde3c9b0746afee9a7376a17b80
DuckonDuck <116538168+papawolfexe@users.noreply.github.com>
2023-04-16 00:30:20 -0700

    Python

--- number_guess_test.py
+++ number_guess_test.py
@@ -4,10 +4,11 @@ numbers = random.randint(1,10)
 
 # Number Guesser
 random_number_guess = int(input("What do you think the random number is?\nrange: (1, 10)\n"))
-if numbers == random_number_guess:
-    print("Congratulations that was the correct number!")
-elif numbers == (random_number_guess + 1) or numbers == (random_number_guess - 1):
+while numbers != random_number_guess:
+    print("Try again")
+    random_number_guess = int(input("What do you think the random number is?\nrange: (1, 10)\n"))
+if numbers == (random_number_guess + 1) or numbers == (random_number_guess - 1):
     print("Very close but no cigar")
 else:
-    print("You suck try again later")
+    print("Good")
     exit()
